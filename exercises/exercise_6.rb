adfd2a08abf72a5fdaa67481079b0a0089e7b077
Clifford Tse <TseClifford@gmail.com>
2022-03-08 11:56:22 -0500

    Exercise 6: One-to-many association

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -8,4 +8,10 @@ require_relative './exercise_5'
 puts "Exercise 6"
 puts "----------"
 
-# Your code goes here ...
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+@store1.employees.create(first_name: "Bobby", last_name: "Bob", hourly_rate: 61)
+@store1.employees.create(first_name: "Johnny", last_name: "John", hourly_rate: 62)
+
+@store2.employees.create(first_name: "Billy", last_name: "Bill", hourly_rate: 63)
+@store2.employees.create(first_name: "Ronny", last_name: "Ron", hourly_rate: 64)
+@store2.employees.create(first_name: "Jimmy", last_name: "Jim", hourly_rate: 65)
